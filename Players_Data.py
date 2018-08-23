37ef3d0e8d8017992a1b66012f9aa7f39ebe69d9
Leo223 <mazarambroz61@gmail.com>
2018-08-23 12:14:26 +0200

    Players_DB v0.0
    - Inicializando estructura del script

---
+++ Players_Data.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import time
+
+class Navegar():
+
+    def __init__(self):
+        self.rutaDrivers = '/home/jcambronero/Escritorio/JCP/Scrape/geckodriver'
+        self.rutaDrivers = '/home/jcambronero/Escritorio/JCP/Scrape/chromedriver'
+        self.browser = webdriver
+
+
+    def Firefox(self):
+        self.browser = self.browser.Firefox(executable_path=self.rutaDrivers)
+        return self.browser
+
+    def Chrome(self):
+        self.options = self.browser.ChromeOptions()
+        self.options.add_argument("headless")
+        self.browser = self.browser.Chrome(executable_path=self.rutaDrivers,chrome_options=self.options)
+        return self.browser
+
+
+    def Login(self):
+        self.browser = self.Chrome()
+        self.url1 = 'https://biwenger.as.com/login'
+        self.params = {'usuario':'mazarambroz61@gmail.com','password':'JuliosPower'}
+        self.browser.get(self.url1)
+
+        self.botones = {
+            'logearse':'/html/body/app-root/main/ng-component/div/div[2]/button[1]',
+            'submit_login':'//*[@id="login-wrapper"]/div[2]/form/button'}
+
+        self.elem = self.browser.find_element_by_xpath(self.botones.get('logearse')).click()
+        self.usuario = self.browser.find_element_by_name("email")
+        self.password = self.browser.find_element_by_name("password")
+        self.usuario.send_keys(self.params.get('usuario'))
+        self.password.send_keys(self.params.get('password'))
+
+        self.entrar = self.browser.find_element_by_xpath(self.botones.get('submit_login'))
+        self.entrar.click()
+
+        time.sleep(3)
+        self.html = self.browser.page_source
+
+        print (self.html)
+
+        # return self.browser
+
+
+
+    def exe(self):
+
+        self.Login()
+
+        # self.browser.close()
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    Navegar().Login()
+
+
+
+
+# browser.close()
+# browser.quit()
